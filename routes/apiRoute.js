77a50faa45dc208c34071e0fa45272eb1ac12834
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-09-08 14:06:52 -0700

    updated the post function as well as the delete function in the apiRoute.js

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,33 +1,32 @@
-/*
-GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
-This means you'll need to find a way to give each note a unique id when it's saved.
-In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-*/
-// const store = require('../db/utilities')
-const notes = require('../db/db.json')
+let notes = require('../db/db.json')
 const router = require('express').Router();
-const uuidv1 = require('uuid/v1');
+const { v1: uuidv1 } = require('uuid');
+const fs = require('fs');
 
 
 router.get('/notes', function (req, res) {
-  res.json(notes)
-  //         store
-  //             .getNotes()
-  //             .then(notes => res.json(notes))
+
+  res.json(notes);
 });
 
 router.post('/notes', function (req, res) {
-  let newNote ={title:req.body.title, text:req.body.text, id:uuidv1()};
-  console.log(newNote);
+  let newNote = { title: req.body.title, text: req.body.text, id: uuidv1() };
+  // console.log(newNote);
   notes.push(newNote);
+  let data = JSON.stringify(notes);
   res.json(newNote);
 });
 
-router.delete('/notes', function (req, res) {
-  res.json(notes)
+router.delete('/notes/:id', function (req, res) {
+  function checkId(note) {
+    return note.id !== req.params.id;
+  }
+  notesTemp = notes.filter(checkId);
+  notes = notesTemp
+  fs.writeFileSync('db/db.json', JSON.stringify(notes))
+  res.json(notes);
 
 });
 
 module.exports = router;
+
